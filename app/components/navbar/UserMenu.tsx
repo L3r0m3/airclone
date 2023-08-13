7ed697ca99d6cbf5b76704b46f90f83ace71ee86
L3r0m3 <lerome_brown@yahoo.com>
2023-08-13 18:44:57 +0200

    On branch master  Your branch is up to date with 'L3r0m3/master'.  Changes to be committed: 	new file:   .env 	new file:   app/actions/getCurrentUser.ts 	new file:   app/api/register/route.ts 	modified:   app/components/Avatar.tsx 	new file:   app/components/Heading.tsx 	new file:   app/components/input/Input.tsx 	new file:   app/components/modals/LoginModal.tsx 	modified:   app/components/modals/Modal.tsx 	modified:   app/components/modals/RegisterModal.tsx 	modified:   app/components/navbar/Navbar.tsx 	modified:   app/components/navbar/UserMenu.tsx 	new file:   app/hooks/useLoginModal.ts 	renamed:    app/hooks/useRegisterModals.ts -> app/hooks/useRegisterModal.ts 	modified:   app/layout.tsx 	new file:   app/libs/prismadb.ts 	new file:   app/provider/ToasterProvider.tsx 	new file:   app/types/index.ts 	modified:   next.config.js 	modified:   package-lock.json 	modified:   package.json 	new file:   pages/api/auth/[...nextauth].ts 	new file:   prisma/schema.prisma

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,10 +4,21 @@ import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar';
 import { useCallback, useState } from 'react';
 import MenuItem from './MenuItem';
-import useRegisterModal from '@/app/hooks/useRegisterModals';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
+import { signOut } from 'next-auth/react';
+import { SafeUser } from '@/app/types';
 
-const UserMenu = () => {
+interface UserMenuProps {
+    currentUser?: SafeUser | null;
+
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({
+    currentUser
+}) => {
     const registerModal = useRegisterModal();
+    const loginModal = useLoginModal();
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleOpen = useCallback(() => {
@@ -54,7 +65,7 @@ const UserMenu = () => {
                 >
                     <AiOutlineMenu />
                     <div className="hidden md:block">
-                        <Avatar />
+                        <Avatar src={currentUser?.image}/>
                     </div>
                 </div>
             </div>
@@ -75,9 +86,41 @@ const UserMenu = () => {
                     "
                 >
                     <div className="flex flex-col cursor-pointer">
+                        {currentUser ? (
+
+                        <>
+                        <MenuItem
+                            onClick={() => {}}
+                            label="My trips"
+                        />
+                        <MenuItem
+                            onClick={() => {}}
+                            label="My favorites"
+                        />
+                        <MenuItem
+                            onClick={() => {}}
+                            label="My reservations"
+                        />
+                        <MenuItem
+                            onClick={() => {}}
+                            label="My properties"
+                        />
+                        <MenuItem
+                            onClick={() => {}}
+                            label="Airbnb Home"
+                        />
+                        <hr/>
+                        <MenuItem
+                            onClick={() => signOut()}
+                            label="Logout"
+                        />
+                        </>
+
+                        ) : (
+
                         <>
                             <MenuItem
-                                onClick={() => {}}
+                                onClick={loginModal.onOpen}
                                 label="Login"
                             />
                             <MenuItem
@@ -85,6 +128,7 @@ const UserMenu = () => {
                                 label="Sign up"
                             />
                         </>
+                        )}
                     </div>
                 </div>
             )}
